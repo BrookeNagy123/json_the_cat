e0720fc774ef3c4780bb72bb0233202f3fe2b70f
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-11-19 13:55:45 -0700

    Refactored to use callbacks

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,24 @@
 const request = require('request');
 
-const args = process.argv;
-const sliced = args.slice(2);
-let breed = sliced[0];
+const fetchBreedDescription = function(breedName, callback) {
+  request("https://api.thecatapi.com/v1/breeds/search?q=" + breedName, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    let data = JSON.parse(body);
+    const newData = data[0];
+    if (newData) {
+      callback(null, newData.description);
+    } else {
+      callback("Breed not found");
+    }
+    // ('statusCode:', response && response.statusCode);
+    // ('body:', body);
+  });
+};
 
-request("https://api.thecatapi.com/v1/breeds/search?q=" + breed, (error, response, body) => {
-  ('error:', error);
-  ('statusCode:', response && response.statusCode);
-  ('body:', body);
-  if (error) {
-    console.log('error:', error);
-  }
-  let data = JSON.parse(body);
-  if (data[0] === undefined) {
-    console.log("We are sorry, we are unable to find that breed");
-  } else {
-    console.log(data[0].description);
-  }
-});
 
+module.exports = { fetchBreedDescription };
 
 
 
